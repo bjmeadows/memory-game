50cd28423ffd5cf25bc2ea7f3769f00abcba8d1e
bjmeadows <branden.meadows@gmail.com>
2018-08-26 02:59:44 -0700

    Final commit: Game is finalized and ready for play

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
 var openCards = [];
 var matchedCards = [];
 var scoreCounter = 0;
-// Shuffle function from http://stackoverflow.com/a/2450976
+var timerValue = 1;
+var timer_on = false;
+var timedStart;
+
 function shuffle(array) {
+  // Shuffle function from http://stackoverflow.com/a/2450976
     var currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
@@ -12,8 +16,31 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
-
     return array;
+};
+
+
+function timerFunction(){
+  //This function creates the timer for the game
+    timedStart = setInterval(function(){
+    $('.timer').html(timerValue);
+    timerValue++;
+    }, 1000);
+};
+
+
+function stopTimer(){
+  //This function stops the timer
+  clearInterval(timedStart);
+};
+
+
+function startTimer(){
+  //This function starts the timer, and is called below with an event listener
+  if (timer_on != true){
+  timer_on = true;
+  timerFunction();
+};
 };
 
 /*
@@ -23,8 +50,9 @@ var cardList = ["fa-diamond", "fa-diamond", "fa-anchor", "fa-anchor", "fa-bomb",
 
 
 
-//this function displays all of the cards in html. It also adds a child DOM element of "i" which will hold the class fa for each card. It was easier to do it this way after deleting it from the html than to input it all in html again.
+
 function displayCards() {
+  //this function displays all of the cards in html. It also adds a child DOM element of "i" which will hold the class fa for each card. It was easier to do it this way after deleting it from the html than to input it all in html again.
   for (var x = 0; x < cardList.length; x++) {
     $(".deck").append('<li class="card"> </li>');
   };
@@ -51,13 +79,23 @@ function shuffleCards() {
 
 shuffleCards()
 
-//Flips cards back to blank state
+
 function flipBack(){
+  //Flips cards back to blank state
   $('.open').toggleClass('open show');
 };
 
-//Restart function that sets all of the card settings back to not open and reshuffles the game
+function resetTimer(){
+//Simple function to reset the timer to 0
+  timerValue = 0;
+  $('.timer').html(timerValue);
+  timer_on = false;
+  stopTimer();
+};
+
+
 $(".restart").on('click', function resetGame(){
+  //Restart function that sets all of the card settings back to not open and reshuffles the game
   $('.card').removeClass('open show');
   $('.card').removeClass('match');
   $('.card i').remove();
@@ -67,15 +105,20 @@ $(".restart").on('click', function resetGame(){
   $('.moves').html(scoreCounter);
   matchedCards = [];
   resetStars();
+  resetTimer();
 });
 
 
 function flipCards(){
-
+  //This function flips the cards over on click when called below
+//The first part of the if statement starts the timer and will open a card when clicked. It will push the opened card to the openCards array to check if there is a match once a second card is clicked
   if (openCards.length === 0) {
       $(this).toggleClass('open show');
       openCards.push($(this));
-    } else if (openCards.length === 1 && (!$(this).hasClass('open show')) && (!$(this).hasClass('match'))) {
+      timeStart = true;
+    }
+//The second part of this if statement will only happen if there is already one card open. It also will not occur if you click the same card twice. Once a second card is opened it will compare whether or not the two cards match, it will increase the score, and will check if all of the cards are matched.
+    else if (openCards.length === 1 && (!$(this).hasClass('open show')) && (!$(this).hasClass('match'))) {
       $(this).toggleClass('open show');
       openCards.push($(this));
       doesMatch();
@@ -108,40 +151,57 @@ function doesMatch(){
 
 
 function resetOpenCards(){
+  //Simple function to return the open cards array to empty.
   openCards = [];
 };
 
-function playGame() {
-  $('.card').on('click', flipCards);
-};
 
 
-//Counts each move and edits the html to show the score
+
 function moveCounted() {
+  //Counts each move and edits the html to show the score
   $('.moves').html(scoreCounter);
 };
 
-//Changes the star rating based on overall score
+
 function starRating() {
-  if (scoreCounter === 15) {
-    $('.stars li .fa.fa-star').last().replaceWith('<i class = "fa fa-star-o"> </i>');
-  } else if (scoreCounter === 20) {
+  //Changes the star rating based on overall score
+  if (scoreCounter === 20) {
     $('.stars li .fa.fa-star').last().replaceWith('<i class = "fa fa-star-o"> </i>');
   } else if (scoreCounter === 25) {
+    $('.stars li .fa.fa-star').last().replaceWith('<i class = "fa fa-star-o"> </i>');
+  } else if (scoreCounter === 30) {
     $('.stars li .fa.fa-star').last().replaceWith('<i class = "fa fa-star-o"> </i>');
   }
 };
 
 function resetStars() {
+//Resets the stars back to 3 stars once the game is reset
   $('.stars li .fa.fa-star-o').replaceWith('<i class = "fa fa-star"> </i>')
+  $('#star-score').html('Stars: ');
 }
 
+
+
+function winChecker(){
+  //checks the length of matchedCards array to see if it equals the same length as cardList. If it does then the won game popup appears and if not then you continue the game
+  if (matchedCards.length === 8) {
+    $('#score-number').html("Final Score: " + scoreCounter + " Moves");
+    $('.stars').clone().appendTo('#star-score');
+    $('#time-score').html('Time: ' + timerValue + " seconds");
+    stopTimer();
+    winPopUp();
+  }
+};
+
 function winPopUp() {
+  //This triggers the modal when you win. If you click play again it resets the game. If you click the "x" button it will close the modal.
   $('.modal').css({display: "block"});
-  $('.close-button').on('click', function resetGame(){
+  $('.play-again').on('click', function resetGame(){
     $('.card').removeClass('open show');
     $('.card').removeClass('match');
     $('.card i').remove();
+    resetTimer();
     resetOpenCards();
     shuffleCards();
     scoreCounter = 0;
@@ -150,15 +210,15 @@ function winPopUp() {
     $('.modal').css({display: 'none'});
     resetStars();
   });
+  $('.close-button').on('click', function closeWindow(){
+    $('.modal').css({display: "none"});
+});
 };
 
-function winChecker(){
-  //checks the length of matchedCards array to see if it equals the same length as cardList. If it does then the won game popup appears and if not then you continue the game
-  if (matchedCards.length === 8) {
-    $('#score-number').html("Final Score: " + scoreCounter + " Moves");
-    $('.stars').clone().appendTo('#star-score');
-    winPopUp();
-  }
+function playGame() {
+  //This function starts the game with an event listener, as well as is the listener to start the timer.
+  $('.card').on('click', flipCards);
+  $('.card').on('click',startTimer);
 };
 
 playGame();
